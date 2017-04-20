51bbda9c48bfc73d2a49e9314ce6287c84d936fe
fmassaretto <fabio_no_more@yahoo.com.br>
2017-04-20 00:01:05 -0300

    First Commit

--- PrimeiroAppXamarin/PrimeiroAppXamarin/MainPage.xaml.cs
+++ PrimeiroAppXamarin/PrimeiroAppXamarin/MainPage.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using PrimeiroAppXamarin.ViewModel;
 
 namespace PrimeiroAppXamarin
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage
     {
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new PrimeiroAppXamarinViewModel();
         }
     }
 }
---
+++ PrimeiroAppXamarin/PrimeiroAppXamarin/ViewModel/PrimeiroAppXamarinViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Xamarin.Forms;
+
+namespace PrimeiroAppXamarin.ViewModel
+{
+    public class PrimeiroAppXamarinViewModel : BaseViewModel
+    {
+        private int _primeiroNumero;
+
+        public int PrimeiroNumero
+        {
+            get { return _primeiroNumero; }
+            set
+            {
+                _primeiroNumero = value;
+                OnPropertyChanged();
+                SomarCommand.ChangeCanExecute();
+            }
+        }
+
+        private int _segundoNumero;
+
+        public int SegundoNumero
+        {
+            get { return _segundoNumero; }
+            set
+            {
+                _segundoNumero = value;
+                OnPropertyChanged();
+                SomarCommand.ChangeCanExecute();
+            }
+        }
+
+        private int _resultado;
+
+        public int Resultado
+        {
+            get { return _resultado; }
+            set
+            {
+                _resultado = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command SomarCommand { get; private set; }
+        public PrimeiroAppXamarinViewModel()
+        {
+            SomarCommand = new Command(ExecuteSomarCommand, CanExecuteSomarCommand);
+        }
+
+        private void ExecuteSomarCommand()
+        {
+            Resultado = PrimeiroNumero + SegundoNumero;
+        }
+
+        private bool CanExecuteSomarCommand()
+        {
+            return PrimeiroNumero >= 1 && SegundoNumero >= 1;
+        }
+    }
+}
